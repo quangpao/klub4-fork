88880364931987793acf05d72aeb42d69d2f1aec
quangpao <102311709+quangpao@users.noreply.github.com>
2022-11-13 22:19:09 +0700

    remove test due to removed database from azure

--- src/test/java/com/database/DatabaseTests.java
+++ src/test/java/com/database/DatabaseTests.java
@@ -12,30 +12,30 @@ import DAO.UserDAO;
 
 public class DatabaseTests {
     
-    @Test
-    @DisplayName("Test database connection")
+//     @Test
+//     @DisplayName("Test database connection")
     void testDatabaseConnection() {
 
         assertEquals(true, UserDAO.checkRegister("database@gmail.com"));
 
     }
 
-    @Test
-    @DisplayName("Insert user")
+//     @Test
+//     @DisplayName("Insert user")
     void testInsertUser() {
         String password = "1292131232";
         assertEquals(true, UserDAO.registerUser("Thử Database", "database@gmail.com", password, Date.valueOf("1999-12-12"), "Hà Nội"));
-    }
-    @Test
-    @DisplayName("Test BCrypt")
+//     }
+//     @Test
+//     @DisplayName("Test BCrypt")
     void testBCrypt() {
         String password = "1292131232";
         assertEquals(true, UserDAO.checkLogin("database@gmail.com", password));
         
     }
 
-    @Test
-    @DisplayName("Delete user")
+//     @Test
+//     @DisplayName("Delete user")
     void testDeleteUser() {
         assertEquals(true, UserDAO.deleteUser(
                 UserDAO.getUserByEmail("database@gmail.com").getUserID()));
